67e7b11b58e59a55ab8cfb98b6034dd1730c236b
Elta3lab <ahmedfouad2020era@gmail.com>
2018-09-08 15:43:36 +0200

    delete files

--- test/controllers/places_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class PlacesControllerTest < ActionDispatch::IntegrationTest
-  test "should get new" do
-    get places_new_url
-    assert_response :success
-  end
-
-end
--- test/controllers/trips_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class TripsControllerTest < ActionDispatch::IntegrationTest
-  test "should get new" do
-    get trips_new_url
-    assert_response :success
-  end
-
-end
